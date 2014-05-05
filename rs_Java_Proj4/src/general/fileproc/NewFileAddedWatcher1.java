12c48876ab6f0a8cffa446f56da8b7059dc60568
golgo8128 <golgo8128@yahoo.co.jp>
2014-05-05 13:45:43 -0700

    ...

---
+++ rs_Java_Proj4/src/general/fileproc/NewFileAddedWatcher1.java
@@ -0,0 +1,24 @@
+package general.fileproc;
+
+import java.io.File;
+import java.util.HashSet;
+
+public class NewFileAddedWatcher1 {
+
+	private final String[] filnam_reg_regexps; // *_nodes.tsv (First check), *_edges.tsv, *_ok.txt
+	private final HashSet<File> ignore_files;
+	private final String check_folder;
+	
+	public NewFileAddedWatcher1(String check_folder, String[] filnam_reg_regexps){
+	
+		
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
