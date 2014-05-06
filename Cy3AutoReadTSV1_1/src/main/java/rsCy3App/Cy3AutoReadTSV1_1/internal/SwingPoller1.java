df3547636dc68fa8016ad224c9bf93d990e25127
golgo8128 <golgo8128@yahoo.co.jp>
2014-05-06 16:11:53 -0700

    Probably complete - Auto Cytoscape TSV import...?

--- Cy3AutoReadTSV1_1/src/main/java/rsCy3App/Cy3AutoReadTSV1_1/internal/SwingPoller1.java
+++ Cy3AutoReadTSV1_1/src/main/java/rsCy3App/Cy3AutoReadTSV1_1/internal/SwingPoller1.java
@@ -6,6 +6,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -25,14 +26,16 @@ public final class SwingPoller1 extends JFrame implements ActionListener{
 	private static final long serialVersionUID = 1L;
 
 	private static final SwingPoller1 instance = new SwingPoller1();
+		
+	private static Timer polltimer;
 	
+	private static JPanel mainPanel;
+	private static JLabel indic_label;
+	private static JButton quit_button;
+	
+	private static int secdbl_from_start;
 	private static final int sec_rythm = 3;
 	
-	private static Timer polltimer;
-	private static JLabel indic_label;
-	private static int secdbl_from_start;
-	private static JButton quit_button;
-		
 	private static RegServiceBag1_4 rSB = null;
 	
 	private SwingPoller1(){
@@ -40,6 +43,8 @@ public final class SwingPoller1 extends JFrame implements ActionListener{
 		set_frame1();
 		
 		indic_label = new JLabel();
+		indic_label.setFont(new Font("Courier New", Font.PLAIN, 11));
+		indic_label.setText("Initializing ...");
         indic_label.setOpaque(true);
         indic_label.setAlignmentX(Component.CENTER_ALIGNMENT);
 		quit_button = new JButton("Quit");
@@ -47,7 +52,7 @@ public final class SwingPoller1 extends JFrame implements ActionListener{
 		
 		quit_button.addActionListener(this);
 		
-		JPanel mainPanel = new JPanel();
+		mainPanel = new JPanel();
 		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
 		mainPanel.add(Box.createRigidArea(new Dimension(1,10)));
 		mainPanel.add(indic_label);
@@ -115,14 +120,24 @@ public final class SwingPoller1 extends JFrame implements ActionListener{
 			NewFileAutoRead1 newfileautoread = NewFileAutoRead1.getInstance(rSB);
 			int num_new_net = newfileautoread.scan_and_read();
 			
-			indic_label.setText(String.format("Polling (%d sec.)", secdbl_from_start));
+			// indic_label.setText(String.format("Polling (%d sec.)", secdbl_from_start));
+			
+			String indicstr = new String();
+			
+			for(int i = 0;i < 4;i ++){
+				if(i < (secdbl_from_start/sec_rythm) % 4)
+					indicstr += ".";
+				else
+					indicstr += " ";
+			}
+			indic_label.setText("Polling " + indicstr);
 			
 			if(num_new_net > 0)
 				indic_label.setBackground(Color.RED);
-			else if((secdbl_from_start / sec_rythm) % 2 ==0)
-				indic_label.setBackground(Color.LIGHT_GRAY);
-			else
-				indic_label.setBackground(Color.YELLOW);
+			else // if((secdbl_from_start / sec_rythm) % 2 ==0)
+				indic_label.setBackground(mainPanel.getBackground()); // Color.LIGHT_GRAY);
+//			else
+//				indic_label.setBackground(Color.YELLOW);
 			secdbl_from_start += sec_rythm;			
 			
 			polltimer.start();
