182a578820143148c813e43f9972dca7bb888517
rsaito <golgo8128@yahoo.co.jp>
2014-10-17 09:28:23 -0700

    ...

---
+++ rsMetabPPI1_12_1/src/main/java/org/cytoscape/rsCy3App/rsMetabPPI1_12_1/internal/AddHygecdf_pvals1.java
@@ -0,0 +1,19 @@
+package org.cytoscape.rsCy3App.rsMetabPPI1_12_1.internal;
+
+import org.cytoscape.model.CyNode;
+
+public class AddHygecdf_pvals1 {
+
+	private RegServiceBag1_4 rSB;
+	
+	public AddHygecdf_pvals1(RegServiceBag1_4 regServiceBag){	
+	
+		this.rSB   = regServiceBag;
+	}
+	
+	public double calc_hygecdf_pval(CyNode protnode){
+		
+		
+	}	
+	
+}
