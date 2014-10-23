a04e6582630640e671ab0517f0a8db0ad5127bb0
golgo8128 <golgo8128@yahoo.co.jp>
2014-10-22 17:55:30 -0700

    ...

---
+++ rs_Java_Proj4/src/calculation/stats/BH_pval1.java
@@ -0,0 +1,16 @@
+package calculation.stats;
+
+import general.usefuls1.SortIdx_simple1_2;
+
+public class BH_pval1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double[] data = { 1.7, -0.3,  2.1,  0.5, 1.7, 2.9, 2.9, 3.7, 2.9 };
+		
+		SortIdx_simple1_2 sidx = new SortIdx_simple1_2(data);
+		
+	}
+
+}
