03b3c8079c7bf2ee18bb967a922614d418463f87
rsaito <golgo8128@yahoo.co.jp>
2014-10-22 23:19:50 -0700

    ...

--- rs_Java_Proj4/src/calculation/stats/BH_pval1.java
+++ rs_Java_Proj4/src/calculation/stats/BH_pval1.java
@@ -4,12 +4,24 @@ import general.usefuls1.SortIdx_simple1_2;
 
 public class BH_pval1 {
 
+	private double[] pvals;
+	
+	public BH_pval1(double[] pvals){
+		
+		this.pvals = pvals;
+		
+		SortIdx_simple1_2 sidx = new SortIdx_simple1_2(pvals);
+		
+		int[] rank = sidx.getRank();
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		double[] data = { 1.7, -0.3,  2.1,  0.5, 1.7, 2.9, 2.9, 3.7, 2.9 };
+		double[] pvalue_ex1 = { 0.3, 0.4, 0.5, 0.2, 0.4,  0.3, 0.1, 0.4, 0.9, 0.1 };
 		
-		SortIdx_simple1_2 sidx = new SortIdx_simple1_2(data);
+		BH_pval1 bhp1 = new BH_pval1(pvalue_ex1);
 		
 	}
 
