af42b54548b462e9c115e7a133bec44628d51299
rsaito <golgo8128@yahoo.co.jp>
2014-10-22 21:13:21 -0700

    ...

--- rs_Java_Proj4/src/general/usefuls1/SortIdx_simple1_2.java
+++ rs_Java_Proj4/src/general/usefuls1/SortIdx_simple1_2.java
@@ -79,9 +79,9 @@ public class SortIdx_simple1_2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		double[] data = { 1.7, -0.3,  2.1,  0.5, 1.7, 2.9, 2.9, 3.7, 2.9 };
+		double[] value1 = { 1.7, -0.3,  2.1,  0.5, 1.7, 2.9, 2.9, 3.7, 2.9 };
 		
-		SortIdx_simple1_2 sidx = new SortIdx_simple1_2(data);
+		SortIdx_simple1_2 sidx = new SortIdx_simple1_2(value1);
 		int[] out;
 		
 		out = sidx.getSortIdx();
