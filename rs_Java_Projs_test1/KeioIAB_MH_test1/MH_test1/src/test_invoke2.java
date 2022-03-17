3fe901ec297e386b28a01522244382e4c3c9d66f
Duke Togo <golgo8128@yahoo.co.jp>
2022-03-17 16:01:39 +0900

    ...

---
+++ rs_Java_Projs_test1/KeioIAB_MH_test1/MH_test1/src/test_invoke2.java
@@ -0,0 +1,14 @@
+
+import emon2.api.MasterHands;
+
+public class test_invoke2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(System.getenv("RS_TRUNK_DIR"));
+		// System.out.println(mh_version);
+		
+	}
+
+}
