6220b7fe2c641b639eef100b8d64753c39304555
Duke Togo <golgo8128@yahoo.co.jp>
2022-03-22 17:13:59 +0900

    ...

--- rs_Java_Projs_test1/KeioIAB_MH_test1/MH_test1/src/test_invoke2.java
+++ rs_Java_Projs_test1/KeioIAB_MH_test1/MH_test1/src/test_invoke2.java
@@ -21,6 +21,37 @@ public class test_invoke2 {
 		java.util.List<AlignmentInfo> alist = mh.getAlignmentList();
 		for(AlignmentInfo each_a : alist) {
 			System.out.println(each_a.getName());
+			mh.exportCsv(
+					each_a.getId(), // resultId
+		            "C:\\Users\\golgo\\Desktop\\tmpmhout1_" + each_a.getName() + ".csv", // csvPath
+		            false, // printZeroValues
+		            true,  // printGroup
+		            true,  // printSession
+		            true,  // printPeakGroupId
+		            true,  // printAnnotationId
+		            true,  // printAnnotationName
+		            true,  // printAveMz
+		            true,  // printAveCorMT
+		            true,  // printN
+		            true,  // printIs
+		            true,  // printCategory
+		            true,  // printTscore
+		            true,  // printPvalue
+		            true,  // printPeakId
+		            true,  // printMz
+		            true,  // printCorMT
+		            true,  // printMT
+		            true,  // printIntensity,
+		            true,  // printArea
+		            true,  // printRelArea
+		            true,  // printSn
+		            true,  // printNoise
+		            true,  // printHeight
+		            true,  // printLeftMT
+		            true,  // printRightMT
+		            true,  // printConc
+		            true   // printTag
+		            );
 		}
 		
 		mh.closeProject();
