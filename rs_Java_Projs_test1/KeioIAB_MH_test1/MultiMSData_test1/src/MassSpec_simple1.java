7aa170ca7bd1cf4c2fe21ebd9f25b60531cb2200
Rintaro Saito <golgo8128@yahoo.co.jp>
2022-04-09 19:56:41 +0900

    ...

--- rs_Java_Projs_test1/KeioIAB_MH_test1/MultiMSData_test1/src/MassSpec_simple1.java
+++ rs_Java_Projs_test1/KeioIAB_MH_test1/MultiMSData_test1/src/MassSpec_simple1.java
@@ -1,7 +1,26 @@
 
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class MassSpec_simple1 {
 
 	public float[] mzs;
 	public int[] intsties;
-	
+
+	public void output_to_file(DataOutputStream fw) {
+
+		try {
+			for(float mz : this.mzs) {
+				fw.writeFloat(mz);
+			}
+			for(int intsty : this.intsties) {
+				fw.writeInt(intsty);
+			}			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
 }
