f0c5dc905200467f30358f71c59cb77b3153720b
Rintaro Saito <golgo8128@yahoo.co.jp>
2023-06-22 14:04:16 +0900

    Auto commit by Windows batch file ...

--- MasterHandsManips/ReadMHCurations1/src/Test_MHCurBase1_2.java
+++ MasterHandsManips/ReadMHCurations1/src/Test_MHCurBase1_2.java
@@ -20,10 +20,20 @@ public class Test_MHCurBase1_2 {
 					resolve("Quant_linear_check_STD").
 					resolve("180426_TOF21_checkout").
 					resolve("180426_C_TOF21.mhs");
-					*/
+
 		Path tmpmhfile = 
 				Paths.get("D:\\Data\\Metabolome\\Test\\J06_0101_plasma\\101_Plasma.mhs");
 
+					*/
+
+		Path tmpmhfile = 
+				Paths.get(System.getenv("RS_PROJ_DIR")).
+				resolve("MasterHands").
+				resolve("Examples").
+				resolve("samplefiles1").
+				resolve("J06_0101_plasma").
+				resolve("101_Plasma.mhs");
+		
 		/*
 		Path tmpoutfolder =
 				Paths.get(System.getenv("RS_TMP_DIR")).
